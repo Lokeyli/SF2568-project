9232dc37075f258e3a19c055d6cbae5b769e17ac
Lokeyli <lokeyli@outlook.com>
2024-04-26 18:41:23 +0200

    add format to args in the shell script

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Too few arguments"
-    echo "Usage: ./test.sh <image-name>"
+    echo "Too few arguments, should be 2"
+    echo "Usage: ./test.sh <image-name> <image-format>"
     exit 1
 fi
 
 # Change `python3` to `python` if your python3.x is alias to `python`
-python3 utils.py img-to-bin "data/in-image/$1.jpg" "data/in-bin/$1.bin"
+python3 utils.py img-to-bin "data/in-image/$1.$2" "data/in-bin/$1.bin"
 
 mpicc -o exec edge-detection.c -lm && 
 mpirun -np 8 exec "data/in-bin/$1.bin" "data/out-bin/$1.bin" &&
-python3 utils.py bin-to-img "data/out-bin/$1.bin" "data/out-image/$1.jpg"
+python3 utils.py bin-to-img "data/out-bin/$1.bin" "data/out-image/$1.$2"
