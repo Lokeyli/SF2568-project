4aa754a001bc0ac533fad2ed7d14cef6ab87528e
Lokeyli <lokeyli@outlook.com>
2024-05-20 08:03:10 +0200

    More and more figures

--- plot.py
+++ plot.py
@@ -26,6 +26,10 @@ plt.savefig("docs/figures/time-taken.png")
 # Clean the plot
 plt.clf()
 
+
+speedups = []
+X = []
+filename_postfix = sys.argv[-1][-1]
 for i in range(1, len(sys.argv)):
     with open(sys.argv[i], "r") as f:
         data = f.readlines()
@@ -36,9 +40,29 @@ for i in range(1, len(sys.argv)):
         # Calculate speedup
         speedup = [y[0] / time for time in y]
         plt.scatter(x, speedup, label=sys.argv[i])
+        speedups.append(speedup)
+        X = x
+# Calculate the average speedup
+average_speedup = np.mean(speedups, axis=0)
+plt.plot(X, average_speedup, label="Average")
+# Calculate the average efficiency, e = speedup / P
+average_efficiency = average_speedup / X
 
 plt.xlabel("Number of Core")
 plt.ylabel("Speedup")
 plt.title("Speedup against Number of Core")
 plt.legend()
-plt.savefig("docs/figures/speedup.png")
+plt.savefig(f"docs/figures/speedup-compare-blocked-nonblocked-{filename_postfix}.png")
+
+# Plot another graph, the efficiency
+# Using P = 1 as the baseline
+
+# Clean the plot
+plt.clf()
+
+plt.plot(X, average_efficiency, label="Average")
+plt.xlabel("Number of Core")
+plt.ylabel("Efficiency")
+plt.title("Efficiency against Number of Core")
+plt.legend()
+plt.savefig("docs/figures/efficiency.png")
